3ff00b2354c0af5626fc09ad0be3df51caafa37f
FahadHussain777 <root@mysite.com>
2018-05-07 11:39:23 +0500

    First commit

---
+++ Mastering/SampleModule/Block/Hello.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Mastering\SampleModule\Block;
+
+use Magento\Framework\View\Element\Template;
+use Mastering\SampleModule\Model\ResourceModel\Item\CollectionFactory;
+
+class Hello extends Template
+{
+    /**
+     * @var CollectionFactory
+     */
+    protected $collectionFactory;
+
+    /**
+     * Hello constructor.
+     * @param CollectionFactory $collectionFactory
+     * @param array $data
+     */
+    public function __construct(
+        Template\Context $context,
+        CollectionFactory $collectionFactory,
+        array $data=[]
+    ){
+        $this->collectionFactory = $collectionFactory;
+        parent::__construct($context,$data);
+    }
+
+    /**
+     * @return collection
+     */
+    public function getItems(){
+        $item=$this->collectionFactory->create()->getItems();
+        return $item;
+    }
+}
---
+++ Mastering/SampleModule/Cron/AddItem.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Mastering\SampleModule\Cron;
+
+
+use Mastering\SampleModule\Model\ItemFactory;
+
+class AddItem
+{
+    private $itemFactory;
+
+    public function __construct(ItemFactory $itemFactory)
+    {
+        $this->itemFactory = $itemFactory;
+    }
+
+    public function execute()
+    {
+        $this->itemFactory->create()
+            ->setName('Schedualed Item')
+            ->setDescription('Created at: '.time())
+            ->save();
+    }
+}
